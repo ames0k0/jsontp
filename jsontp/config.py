bf4a07ca542ba0834564b38a499609a4706bf623
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-02-26 23:53:43 +0300

    system: Auto indent

--- jsontp/config.py
+++ jsontp/config.py
@@ -2,23 +2,23 @@
 
 
 class FD:
-  """File and Directory
-  """
-  ROOT_DIRECTORY = 'data'
-  FILE_EXTENSION = '.json'
-  STDOUT = '*'
+    """File and Directory"""
+
+    ROOT_DIRECTORY = "data"
+    FILE_EXTENSION = ".json"
+    STDOUT = "*"
 
 
 class Tree:
-  ROOT = 'root'
-  DELIMITER = ' -> '
-  LIST_INDEX_WRAPPER = "[{index}]"
-  SEARCH_LIMIT = -1
-  SEARCH_ITEMS_LIMIT = -1
-  SEARCH_FILTER_KEY = ''
-  SEARCH_FILTER_VALUE = ''
+    ROOT = "root"
+    DELIMITER = " -> "
+    LIST_INDEX_WRAPPER = "[{index}]"
+    SEARCH_LIMIT = -1
+    SEARCH_ITEMS_LIMIT = -1
+    SEARCH_FILTER_KEY = ""
+    SEARCH_FILTER_VALUE = ""
 
 
 class Key:
-  SHOW = 'print'
-  SAVE = 'return'
+    SHOW = "print"
+    SAVE = "return"
